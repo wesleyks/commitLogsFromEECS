8febd5ee33c7a7365c45f9cb2bb9c09b18fdf0eb
wesleyks <sunwes@gmail.com>
2013-11-17 20:16:47 -0600

    added infinite scrolling

--- commitLogsFromEECS/static/js/main.js
+++ commitLogsFromEECS/static/js/main.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
 	var lastPage = 0,
 		row = $('<div class="row commit"><div class="small-2 large-2 columns avatar"><img width="75px"></div><div class="small-3 large-3 columns timestamp"><p></p></div><div class="small-7 large-7 columns message"><p></p></div></div>');
-	$.ajax('/commits/jsonPageAfter/?page=' + (++lastPage), {
-		'async': false,
-		'dataType': 'json',
-		'success': function(data) {
-			console.log(data);
-			for (var i = 0; i < data.length; i++) {
-				var newRow = row.clone(),
-					timestamp = moment(data[i].timestamp);
-				$('img', newRow).attr('src', data[i].avatar);
-				$('.timestamp p', newRow).text(timestamp.format('MM/DD/YY, h:mm:ss A'));
-				$('.message p', newRow).text(data[i].message);
-				$('.commits').append(newRow);
-			}
+	$('.commits').infinitescroll({
+		navSelector: 'div.pagination',
+		nextSelector: 'div.pagination a:first',
+		loading: {
+			msg: $('<div></div>')
+		},
+		state: {
+			isDone: false,
+			currPage: 0
+		},
+		animate: false,
+		dataType: 'json',
+		appendCallback: false,
+		prefill: true,
+		path: function(page) {
+			return '/commits/jsonPageAfter/?page=' + page;
+		}
+	}, function(data, opts) {
+		console.log(data);
+		if (data.length === 0) {
+			opts.state.isDone = true;
+		}
+		for (var i = 0; i < data.length; i++) {
+			var newRow = row.clone(),
+				timestamp = moment(data[i].timestamp);
+			$('img', newRow).attr('src', data[i].avatar);
+			$('.timestamp p', newRow).text(timestamp.format('MM/DD/YY, h:mm:ss A'));
+			$('.message p', newRow).text(data[i].message);
+			$('.commits').append(newRow);
 		}
 	});
 });
